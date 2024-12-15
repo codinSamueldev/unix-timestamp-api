ce84ed879cd3d80bc4bd0a6e7ebe1ba191e40554
Samuel Henao <codinsamueldev@gmail.com>
2024-12-15 01:13:16 -0500

    feat: create feature that let users enter birth date to calculate their age in seconds

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -17,6 +17,38 @@ templates = Jinja2Templates(directory=os.path.abspath("templates"))
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse(request=request, name="index.html", context={})
+
+
+@app.post("/", response_class=HTMLResponse)
+def calculate_how_much_seconds_someone_has_lived(request: Request, birthday: str = Form(...)):
+    """
+    Feature for users to get out of curiosity, how many seconds they have lived.
+    Params:
+        birthday -> param that receives the data entered by the user in the date form.
+
+    expected:
+        200_response -> The feature should be able to calculate since EPOCH, how many seconds
+        an user has lived and return either the seconds or age, or both.
+
+    To keep in mind:
+        a not leap year has 31536000 seconds.
+
+    """
+
+    birthday_date = datetime.strptime(birthday, "%Y-%m-%d") # get user birth date, turn it into a datetime obj.
+    
+    current_seconds = int(datetime.now().timestamp()) # get how many seconds have transcurred since EPOCH when POST request made.
+    user_birthday_unix_seconds = int(birthday_date.timestamp()) # calculate seconds since EPOCH until user birth date.
+
+    user_age_in_seconds = current_seconds - user_birthday_unix_seconds # substract and get user lived seconds.
+    
+    user_is = user_age_in_seconds // 31536000 # how old user is?
+
+
+    return templates.TemplateResponse(request=request, name="results.html", context={ 
+        "seconds": user_age_in_seconds,
+        "year": user_is
+        })
 
 
 @app.get("/api")
